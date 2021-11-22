c0a41f2d5e60e70957f11d217dbef4242f477232
AbhishekSagu <abhishek.sagu@antiersolutions.com>
2021-11-22 12:30:01 +0530

    added Header component

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
 import Head from "next/head";
+import Header from "../components/Header/Header";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Airbnb-clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1>HELLO AIRBNB</h1>
-      </main>
+      <div>
+        {/* HEADER */}
+        <Header />
+
+        {/* BANNER */}
+
+        {/* FEED */}
+      </div>
     </div>
   );
 }

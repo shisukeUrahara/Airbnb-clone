9ffc45706cb48581c0a469e6208ceb6c1afba616
AbhishekSagu <abhishek.sagu@antiersolutions.com>
2021-11-23 12:10:27 +0530

    added mainBOttom section component

--- components/MainBottomSection/MainBottomSection.js
+++ components/MainBottomSection/MainBottomSection.js
@@ -1,9 +1,16 @@
 import React from "react";
+import LargeCard from "../LargeCard/LargeCard";
 
 function MainBottomSection() {
   return (
     <div>
       <h1>hello main bottom</h1>
+      <LargeCard
+        img="https://links.papareact.com/4cj"
+        title="The greatest outdoors"
+        description="Wishlists curated by Airbnb"
+        buttonText="Get inspired"
+      />
     </div>
   );
 }
